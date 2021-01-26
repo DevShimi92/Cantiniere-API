5d516c55a8c65763d70c1bbabfa349022f29a5d8
DevShimi92 <hospitalier.philippe@gmail.com>
2021-01-26 17:15:57 +0100

    Update index.ts

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,21 +8,6 @@ import {moduleOrderInfo, moduleDeleteOrderInfo}  from './src_test/module_test_or
 
 import { sequelize } from "../config/database";
 import { log } from "../config/log_config";
-
-before(function(done) {
-    
-        log.info('Preparation of the test base');
-    
-        sequelize.sync().then(() => {
-          log.info('Synchronisation de la base réussi !');
-          done();
-          }).catch(err => {
-          log.error('Erreur lors de la synchronisation de la base de donnée !');
-          log.error(err);
-          done();
-        });
-
-      });
     
 describe('Test of API', function() {
 
