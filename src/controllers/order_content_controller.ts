ac31521056b1f2743a3689bd45d6122996fa995f
DevShimi92 <hospitalier.philippe@gmail.com>
2021-01-30 15:40:42 +0100

    Update order_content_controller.ts

--- src/controllers/order_content_controller.ts
+++ src/controllers/order_content_controller.ts
@@ -58,14 +58,12 @@ export class OrderContentController {
 
     if ( req.body.id_article == null || req.body.id_order == null) 
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Add Article to Order : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_article) || isNaN(req.body.id_order) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Add Article to Order : Fail - The value is not number"); 
       }
     else
