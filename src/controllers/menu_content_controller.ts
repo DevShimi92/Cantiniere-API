6ad50d1e0a2a451fedbcb5a0cc4beb1566dca142
DevShimi92 <39488884+DevShimi92@users.noreply.github.com>
2021-12-07 16:48:16 +0100

    update 0.16.4v
    - Update doc

--- src/controllers/menu_content_controller.ts
+++ src/controllers/menu_content_controller.ts
@@ -6,13 +6,91 @@ import { MenuContent } from "../models/menu_content";
 
 export class MenuContentController {
 
+  /**
+   * @apiDefine admin Canteen manager only
+   * Need an account with the Canteen manager access 
+   */
+
+  /**
+   * @apiDefine MenuContentError
+   *
+   * @apiError (500 Internal Server Error) InternalServerError The server encountered an unexpected error.
+   * 
+   * @apiErrorExample 500-Error-Response :
+   *     HTTP/1.1 500 Internal Server Error
+   */
+
+  /**
+   * @api {get} /menu/content/:id_menu Get Menu Content
+   * @apiName GetMenuContent
+   * @apiGroup MenuContent
+   * 
+   * @apiParam  {Number} id_menu   ID du menu.
+   * 
+   * @apiParamExample {string} Request-Example:
+   *     /menu/content/1
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data                        List of menu content(Array of Objects).
+   * @apiSuccess {Number}   data.id                     ID of menu content.
+   * @apiSuccess {Number}   data.id_menu                ID of menu.
+   * @apiSuccess {Number}   data.id_article             ID of article.
+   * @apiSuccess {String}   data.MenuInfo_Name          Name of menu.
+   * @apiSuccess {String}   data.MenuInfo_Description   Description of menu.
+   * @apiSuccess {Number}   data.MenuInfo_Price_final   Price final of menu.
+   * @apiSuccess {String}   data.Article_Name           Name of article.
+   * @apiSuccess {Number}   data.Article_Code_type_src  Code type of article.
+   * @apiSuccess {Number}   data.Article_Price          Price of article.
+   * @apiSuccess {String}   data.Article_Picture        Picture of article.
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *      [{
+   *           "id": 1,
+   *           "id_menu": 1,
+   *           "id_article": 1,
+   *           "MenuInfo.name": "Menu",
+   *           "MenuInfo.description": null,
+   *           "MenuInfo.price_final": 0,
+   *           "Article.name": "Coca-cola",
+   *           "Article.code_type_src": 1,
+   *           "Article.price": 1.5,
+   *           "Article.picture": null
+   *       },
+   *       {
+   *           "id": 1,
+   *           "id_menu": 1,
+   *           "id_article": 2,
+   *           "MenuInfo.name": "Menu",
+   *           "MenuInfo.description": null,
+   *           "MenuInfo.price_final": 0,
+   *           "Article.name": "Baguette",
+   *           "Article.code_type_src": 2,
+   *           "Article.price": 1,
+   *           "Article.picture": "https://urlToPictureOFaPain.com/image/baguette.jpeg"
+   *       }]
+   * 
+   * 
+   * @apiError {String} NumberOnly	The value <code>id_menu</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse MenuContentError
+   */
   public async getMenu(req: Request,res: Response) : Promise<void> {
     log.info("Get Menu")
     
     if (!Number(req.params.id_menu))
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Get Menu : Fail - The value is not number"); 
       }
     else
@@ -50,6 +128,38 @@ export class MenuContentController {
 
   }
 
+  /**
+   * @api {post} /menu Post Menu Content
+   * @apiName PostMenuContent
+   * @apiGroup MenuContent
+   * @apiPermission admin
+   * 
+   * @apiBody {Number} id_article   ID of article.
+   * @apiBody {Number} id_menu      ID of menu.
+   * 
+   * @apiSuccess (Success 204) NoContent Update of content of menu done.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly	The value <code>id_article</code> or <code>id_menu</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse MenuContentError
+   */
   public async addToMenu(req: Request,res: Response) : Promise<void> {
     log.info("Add Article to Menu");
 
@@ -78,19 +188,56 @@ export class MenuContentController {
     }
   }
 
+  /**
+   * @api {delete} /menu Delete Menu Content
+   * @apiName DeleteMenuContent
+   * @apiGroup MenuContent
+   * @apiPermission admin
+   * 
+   * @apiBody {Number} id           ID of menu content.
+   * @apiBody {Number} id_article   ID of article.
+   * @apiBody {Number} id_menu      ID of menu.
+   * 
+   * @apiSuccess (Success 204) NoContent Update of content of menu done.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly	The value <code>id</code> or <code>id_article</code> or <code>id_menu</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Menu Content not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * 
+   * @apiUse MenuContentError
+   */
   public async deleteToMenu(req: Request,res: Response) : Promise<void> {
     log.info("Delete Article to Menu");
 
     if ( req.body.id_article == null || req.body.id_menu == null || req.body.id == null) 
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Delete Article to Menu : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_article) || isNaN(req.body.id_menu) || isNaN(req.body.id) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Delete Article to Menu : Fail - The value is not number"); 
       }
     else
--- src/controllers/menu_info_controller.ts
+++ src/controllers/menu_info_controller.ts
@@ -28,19 +28,71 @@ async function compareAndUpdate(id:number, value:string, ColToChange:string) {
 
 export class MenuInfoController {
 
+  /**
+   * @apiDefine admin Canteen manager only
+   * Need an account with the Canteen manager access 
+   */
+
+  /**
+   * @apiDefine MenuError
+   *
+   * @apiError (500 Internal Server Error) InternalServerError The server encountered an unexpected error.
+   * 
+   * @apiErrorExample 500-Error-Response :
+   *     HTTP/1.1 500 Internal Server Error
+   * 
+   */
+
+  /**
+   * @api {post} /menu Create  Menu 
+   * @apiName PostMenu 
+   * @apiGroup Menu
+   * @apiDescription Warning : This request must be send in **content-type**.
+   * @apiPermission admin
+   * 
+   * @apiBody {String} name            Name of menu.
+   * @apiBody {Number} [price_final]   Price of menu.
+   * @apiBody {String} [description]   Description of menu.
+   * @apiBody {File}   [img]           Image of menu.
+   * 
+   * @apiSuccess (Success 201) {Number} Id of Menu created.
+   * 
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 201 Created
+   *     {
+   *       id : 1
+   *     }
+   * 
+   * @apiError {String} MissingFields The fields <code>name</code> are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly	The value <code>price_final</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse MenuError
+   * 
+   */
   public async createMenu(req: Request, res: Response) : Promise<void> {
     log.info("Create Menu");
 
     if (req.body.name == null )
       {
-            res.status(400).json({ error : 'Missing Fields' });
-            res.end();
+            res.status(400).json({ error : 'Missing Fields' }).end();
             log.error("Create Menu : Fail - Missing Fields");      
       }
     else if( req.body.price_final != null && isNaN(req.body.price_final))
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Create Menu : Fail - The value of price_final is not number"); 
       }
     else
@@ -69,7 +121,7 @@ export class MenuInfoController {
                     {
                       log.info("Create Menu : Image found. Upload image");
                       ImageController.imageProcessing(data.id,res,false);
-                      res.status(200).json({ id : data.get('id')}).end();
+                      res.status(201).json({ id : data.get('id')}).end();
                       log.info("Create Menu : OK");
                     }
                 })
@@ -82,7 +134,37 @@ export class MenuInfoController {
     
   }
 
-  public async getAllMenu(req: Request,res: Response) : Promise<void> {
+  /**
+   * @api {post} /menu Get all Menu 
+   * @apiName GetMenu 
+   * @apiGroup Menu
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data                  List of menu (Array of Objects).
+   * @apiSuccess {Number}   data.id               ID of menu.
+   * @apiSuccess {String}   data.name             Name of menu.    
+   * @apiSuccess {Number}   data.price_final      Price final of menu.   
+   * @apiSuccess {String}   data.picture          Picture of menu.  
+   * @apiSuccess {String}   data.description      Description of menu.
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *     [{ 
+   *            "id": 1, 
+   *            "name": 'Menu', 
+   *            "price_final": 0, 
+   *            "description": null, 
+   *            "picture": null
+   *      }]
+   * 
+   * @apiUse MenuError
+   * 
+   */
+  public async getAllMenu(_req: Request,res: Response) : Promise<void> {
     log.info("Get all Menu");
 
     await MenuInfo.findAll<MenuInfo>({
@@ -109,19 +191,60 @@ export class MenuInfoController {
 
   }
 
+  /**
+   * @api {put} /menu Put Menu 
+   * @apiName PutMenu 
+   * @apiGroup Menu
+   * @apiPermission admin
+   *  
+   * @apiBody {Number} id                ID of menu.
+   * @apiBody {String} [name]            Name of menu.
+   * @apiBody {Number} [price_final]     Price of menu.
+   * @apiBody {String} [description]     Description of menu.
+   * 
+   * @apiSuccess (Success 204) NoContent Update of menu done.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {string} MissingFields The value <code>id</code> is missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response :
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {string} NumberOnly The value <code>id</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response :
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Menu not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * @apiError Conflict Some Fields can't update.
+   * 
+   * @apiErrorExample 409-Error-Response :
+   *     HTTP/1.1 409 Conflict
+   *
+   */
   public async updateMenu(req: Request, res: Response) : Promise<void> {
     log.info("Update Menu");
     
     if ( req.body.id == null )
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Update Menu : Fail - Missing Fields");      
       }
       else if ( isNaN(req.body.id) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Update Menu : Fail - The value is not number"); 
       }
     else
@@ -138,8 +261,7 @@ export class MenuInfoController {
 
       if (idSearch.length == 0)
         {
-          res.status(404).json();
-          res.end();
+          res.status(404).json().end();
           log.error("Update Menu : Fail - Menu not exist");      
         }
       else
@@ -152,15 +274,13 @@ export class MenuInfoController {
 
         if(!errorUpdate)
           {
-            res.status(204);
-            res.end();
+            res.status(204).end();
             log.info("Update Menu : OK");
             UpdateOk=0;
           }
         else
           {
-            res.status(409);
-            res.end();
+            res.status(409).end();
             log.warn("Update Menu : OK with error - "+UpdateOk+' update done only');
             UpdateOk=0;
             errorUpdate=false;
@@ -172,19 +292,54 @@ export class MenuInfoController {
 
   }
 
+  /**
+   * @api {delete} /menu Delete Menu 
+   * @apiName DeleteMenu 
+   * @apiGroup Menu
+   * @apiPermission admin
+   *  
+   * @apiBody {Number} id   ID of menu.
+   * 
+   * @apiSuccess (Success 204) NoContent Menu deleted.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {string} MissingFields The value <code>id</code> is missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response :
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {string} NumberOnly The value <code>id</code> is not number.
+   * 
+   * @apiErrorExample {json} 400-Error-Response :
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Menu not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * @apiUse MenuError
+   * 
+   */
   public async deleteMenu(req: Request,res: Response) : Promise<void> {
     log.info("Delete Menu");
 
     if ( req.body.id == null )
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Delete Menu : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Delete Menu : Fail - The value is not number"); 
       }
     else
