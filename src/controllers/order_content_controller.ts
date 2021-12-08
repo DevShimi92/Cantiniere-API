4e26abe0415ffe2fb74ad0393693db2006059cd7
DevShimi92 <39488884+DevShimi92@users.noreply.github.com>
2021-12-08 22:23:16 +0100

    update 0.16.5v
    - Update doc
    - Slight drop in smell code rate

--- src/controllers/order_content_controller.ts
+++ src/controllers/order_content_controller.ts
@@ -8,13 +8,93 @@ import { Sequelize } from "sequelize";
 
 export class OrderContentController {
 
+  /**
+   * @apiDefine admin Canteen manager only
+   * Need an account with the Canteen manager access 
+   */
+
+   /**
+   * @apiDefine OrderContentFatalError
+   *
+   * @apiError (500 Internal Server Error) InternalServerError The server encountered an unexpected error.
+   * 
+   * @apiErrorExample 500-Error-Response :
+   *     HTTP/1.1 500 Internal Server Error
+   */
+
+  /**
+   * @api {get} /order/:id_client Get Order Content
+   * @apiName GetOrderContent
+   * @apiGroup OrderContent
+   * 
+   * @apiParam {Number} id_order ID Order Info.
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data List of Order Info (Array of Objects).
+   * @apiSuccess {Number}   data.price                            Actual price of article in case of modification (otherwise is null)..
+   * @apiSuccess {Number}   data.origin_price                     Original price of article in case of modification.
+   * @apiSuccess {Number}   data.discout                          Discout on the price of article.
+   * @apiSuccess {Number}   data.OrderInfo_id_client              ID Client.
+   * @apiSuccess {Number}   data.OrderInfo_sold_before_order      The sold of client before order.
+   * @apiSuccess {Number}   data.OrderInfo_total                  The total of order.
+   * @apiSuccess {Number}   data.Article_name                     Name of article.
+   * @apiSuccess {Number}   data.Article_code_type_src            Code type used for this article.
+   * @apiSuccess {Number}   data.Article_price                    Price of article.
+   * @apiSuccess {Number}   data.MenuInfo_id                      ID Menu Info.
+   * @apiSuccess {Number}   data.MenuInfo_name                    Name of menu.
+   * @apiSuccess {Number}   data.MenuInfo_price_final             Price final of menu.
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *      [{
+   *           "price": null,
+   *           "origin_price": null,
+   *           "discout": 0,
+   *           "OrderInfo.id_client": 1,
+   *           "OrderInfo.sold_before_order": 200,
+   *           "OrderInfo.total": 2.5,
+   *           "Article.name": "Coca-cola",
+   *           "Article.code_type_src": 1,
+   *           "Article.price": 1.5,
+   *           "MenuInfo.id": null,
+   *           "MenuInfo.name": null,
+   *           "MenuInfo.price_final": null
+   *         },{
+   *           "price": null,
+   *           "origin_price": null,
+   *           "discout": 0,
+   *           "OrderInfo.id_client": 1,
+   *           "OrderInfo.sold_before_order": 200,
+   *           "OrderInfo.total": 2.5,
+   *           "Article.name": "Baguette",
+   *           "Article.code_type_src": 2,
+   *           "Article.price": 1,
+   *           "MenuInfo.id": null,
+   *           "MenuInfo.name": null,
+   *           "MenuInfo.price_final": null
+   *         }]
+   * 
+   * @apiError {String} NumberOnly The value <code>id_order</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse OrderContentFatalError
+   * 
+   */
   public async getOrderContent(req: Request,res: Response) : Promise<void> {
     log.info("Get Order Content")
 
     if (!Number(req.params.id_order))
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Get Order Content : Fail - The value is not number"); 
       }
 
@@ -31,7 +111,7 @@ export class OrderContentController {
         },
       }).then(function(dataOrderContent) { 
 
-      if(dataOrderContent.length == 0) // dataOrderContent beacause sonarcloud logic
+      if(dataOrderContent.length == 0)
         {
           res.status(204).end();
         }
@@ -50,6 +130,40 @@ export class OrderContentController {
 
   }
 
+  /**
+   * @api {post} /order/content Post Order Content
+   * @apiName PostOrderContent
+   * @apiGroup OrderContent
+   * 
+   * @apiBody {Number} id_order    ID Order Info.
+   * @apiBody {Number} id_article  ID Article.
+   * @apiBody {Number} [id_menu]   ID Menu Info.
+   * 
+   * 
+   * @apiSuccess (Success 204) NoContent OrderContent added.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly The value <code>id_order</code>, <code>id_article</code>or <code>id_menu</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse OrderContentFatalError
+   * 
+   */
   public async addToOrder(req: Request,res: Response) : Promise<void> {
     log.info("Add Article to Order");
 
@@ -78,6 +192,46 @@ export class OrderContentController {
     }
   }
 
+  /**
+   * @api {delete} /order/content Delete Order Content
+   * @apiName DeleteOrderContent
+   * @apiGroup OrderContent
+   * @apiPermission admin
+   * 
+   * @apiBody {Number} id_order    ID Order Info.
+   * @apiBody {Number} id_article  ID Article.
+   * @apiBody {Number} id_menu   ID Menu Info.
+   * 
+   * 
+   * @apiSuccess (Success 204) NoContent OrderContent deleted.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly The value <code>id_order</code>, <code>id_article</code>or <code>id_menu</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Order Content not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * @apiUse OrderContentFatalError
+   * 
+   */
   public async deleteToOrder(req: Request,res: Response) : Promise<void> {
     log.info("Delete Article to Order");
 
@@ -85,14 +239,12 @@ export class OrderContentController {
 
     if ( req.body.id_order == null || (req.body.id_article == null && req.body.id_menu == null )) 
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Delete Article to Order : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_order) || (isNaN(req.body.id_article) && req.body.id_article!=null) || (isNaN(req.body.id_menu) && req.body.id_menu!=null) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Delete Article to Order : Fail - The value is not number"); 
       }
     else
@@ -107,7 +259,7 @@ export class OrderContentController {
             id_order: req.body.id_order,
             id_menu: id_menu,
           }
-        }).then(function(dataOrderContent) {  // dataOrderContent beacause sonarcloud logic
+        }).then(function(dataOrderContent) { 
           if(dataOrderContent == 0)
             {
               res.status(404).end();
@@ -127,15 +279,61 @@ export class OrderContentController {
       }
   }
 
+  /**
+   * @api {get} /orderRecap/:date Get Recap Order
+   * @apiName GetRecapOrder
+   * @apiGroup OrderContent
+   * @apiPermission admin
+   * 
+   * @apiParam {Date} date Date of order.
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data                  List of Order (Array of Objects).
+   * @apiSuccess {Number}   data.id_article               ID of article.
+   * @apiSuccess {Number}   data.nombre                   Number of article in total for the day.
+   * @apiSuccess {String}   data.Article_name             Name of article.
+   * @apiSuccess {Date}     data.OrderInfo_date_order     Date of article. 
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *     [{ 
+   *             "id_article" : 1 ,
+   *             "nombre": 1, 
+   *             "Article.name" : 'Coca-cola', 
+   *             "OrderInfo.date_order": "2021-12-08"
+   *       },
+   *      { 
+   *             "id_article" : 2 ,
+   *             "nombre": 1, 
+   *             "Article.name" : 'Baguette', 
+   *             "OrderInfo.date_order": "2021-12-08"
+   *       }]
+   * 
+   * 
+   * @apiError {String} MissingFields The date is missing or the date is not formated.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields or Bad format"
+   *     }
+   * 
+   * @apiUse OrderContentFatalError
+   * 
+   */
   public async recapOrder(req: Request,res: Response) : Promise<void> {
     log.info("Get Recap Order for one day");
 
-    var regexp = RegExp("^([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])$");
+    let exp = RegExp("^([\\d][\\d][\\d][\\d]-[\\d][\\d]-[\\d][\\d])$");
+    let regexp = new RegExp (exp);
 
     if ( !regexp.test(req.params.date) || req.params.date.length != 10 ) 
       {
-            res.status(400).json({ error : "Missing Fields or Bad format" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields or Bad format" }).end();
             log.error("Get All Order for one day : Fail - Missing Fields or Bad format");      
       }
     else
--- src/controllers/order_info_controller.ts
+++ src/controllers/order_info_controller.ts
@@ -50,19 +50,105 @@ function erreurToSend(res: Response, dataNotOk:boolean, canOrder:boolean, NBOrde
 
 export class OrderInfoController {
 
+   /**
+   * @apiDefine admin Canteen manager only
+   * Need an account with the Canteen manager access 
+   */
+
+   /**
+   * @apiDefine OrderInfoFatalError
+   *
+   * @apiError (500 Internal Server Error) InternalServerError The server encountered an unexpected error.
+   * 
+   * @apiErrorExample 500-Error-Response :
+   *     HTTP/1.1 500 Internal Server Error
+   */
+
+  /**
+   * @api {post} /order Create Order Info
+   * @apiName PostOrderInfo
+   * @apiGroup OrderInfo
+   * 
+   * @apiBody {Number} id_client ID Client.
+   * @apiBody {Date} date_order Date of Order.
+   * @apiBody {Number} sold_before_order The sold of client before order.
+   * @apiBody {Number} total The total of order.
+   * 
+   * @apiSuccess (Success 200) {Number} id ID of Order Info.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 200 OK
+   *      {
+   *         id : 1
+   *      }
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly The value <code>id_client</code>, <code>sold_before_order</code> or <code>total</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError ProblemWithOrder Unauthorized order date or Data user not found.
+   * 
+   * @apiErrorExample 403-Error-Response :
+   *     HTTP/1.1 403 Forbidden
+   * 
+   * @apiError TimeExceeded Order time exceeded.
+   * 
+   * @apiErrorExample 403-Error-Response :
+   *     HTTP/1.1 403 Forbidden
+   *      {
+   *        "error": "Order time exceeded"
+   *      }
+   * 
+     * @apiError LimitExceeded Limit Order exceeded.
+   * 
+   * @apiErrorExample 403-Error-Response :
+   *     HTTP/1.1 403 Forbidden
+   *      {
+   *        "error": "Limit Order exceeded"
+   *      }
+   * 
+   * @apiError LimitExceededAccount Order for this account exceeded.
+   * 
+   * @apiErrorExample 403-Error-Response :
+   *     HTTP/1.1 403 Forbidden
+   *      {
+   *        "error": "Limit Order for this account exceeded"
+   *      }
+   * 
+   * @apiError ProblemWithBalance Insufficient balance or balance incorrect.
+   * 
+   * @apiErrorExample 403-Error-Response :
+   *     HTTP/1.1 403 Forbidden
+   *      {
+   *        "error": "Insufficient balance or balance incorrect"
+   *      }
+   * 
+   * @apiUse OrderInfoFatalError
+   * 
+   */
   public async createOrder(req: Request,res: Response) : Promise<void> {
     log.info("Create Order");
 
     if ( req.body.id_client == null || req.body.sold_before_order == null || req.body.total == null) 
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Create Order : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_client) || isNaN(req.body.sold_before_order) || isNaN(req.body.total))
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Create Order : Fail - The value is not number"); 
       }
     else
@@ -128,13 +214,50 @@ export class OrderInfoController {
     }
   }
 
+  /**
+   * @api {post} /order/:id_client Get Order Info
+   * @apiName GetOrderInfo
+   * @apiGroup OrderInfo
+   * 
+   * @apiParam {Number} id_client ID Client.
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data List of Order Info (Array of Objects).
+   * @apiSuccess {Number}   data.id          ID of Order Info.
+   * @apiSuccess {Date}     data.createdAt   Date of create of order info.
+   * @apiSuccess {Number}   data.total       Total of order.
+   * @apiSuccess {boolean}  data.done        Status of order. 
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *      [{
+   *         id : 1,
+   *         createdAt : '2021-12-08T15:27:27.245Z',
+   *         total : 1,
+   *         done : false
+   *      }]
+   * 
+   * @apiError {String} NumberOnly The value <code>id_client</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiUse OrderInfoFatalError
+   * 
+   */
   public async getOrder(req: Request,res: Response) : Promise<void> {
     log.info("Get Order")
 
      if (!Number(req.params.id_client))
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Get Order : Fail - The value is not number"); 
       }
     else
@@ -166,19 +289,54 @@ export class OrderInfoController {
       }
   }
 
+  /**
+   * @api {delete} /order Delete Order Info
+   * @apiName DeleteOrderInfo
+   * @apiGroup OrderInfo
+   * @apiPermission admin
+   * 
+   * @apiParam {Number} id_order ID Order Info.
+   * 
+   * @apiSuccess (Success 204) NoContent Order Info deleted.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly The value <code>id_order</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Order Info not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * @apiUse OrderInfoFatalError
+   * 
+   */
   public async deleteOrder(req: Request,res: Response) : Promise<void> {
     log.info("Delete Order");
 
     if ( req.body.id_order == null )
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Delete Order : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_order) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Delete Order : Fail - The value is not number"); 
       }
     else
@@ -246,20 +404,55 @@ export class OrderInfoController {
       } 
   }
 
+  /**
+   * @api {put} /order/valid Put Valid Order
+   * @apiName PutValidOrder
+   * @apiGroup OrderInfo
+   * @apiPermission admin
+   * 
+   * @apiBody {Number} id_order ID Order Info.
+   * 
+   * @apiSuccess (Success 204) NoContent Order Info deleted.
+   * 
+   * @apiSuccessExample Success-Response :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiError {String} MissingFields Some fields are missing.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields"
+   *     }
+   * 
+   * @apiError {String} NumberOnly The value <code>id_order</code> is not number.
+   *   
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Number only"
+   *     }
+   * 
+   * @apiError NotExist Order Info not exist.
+   * 
+   * @apiErrorExample 404-Error-Response :
+   *     HTTP/1.1 404 Not Found
+   * 
+   * @apiUse OrderInfoFatalError
+   * 
+   */
   public async validOrder(req: Request,res: Response): Promise<void>{
 
     log.info("Valid Order");
 
     if ( req.body.id_order == null )
       {
-            res.status(400).json({ error : "Missing Fields" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields" }).end();
             log.error("Valid Order : Fail - Missing Fields");      
       }
     else if ( isNaN(req.body.id_order) )
       {
-            res.status(400).json({ error : "Number only" });
-            res.end();
+            res.status(400).json({ error : "Number only" }).end();
             log.error("Valid Order : Fail - The value is not number"); 
       }
     else
@@ -314,15 +507,57 @@ export class OrderInfoController {
 
   }
 
+  /**
+   * @api {get} /orderRecap/list/:date Get All Order For One Day
+   * @apiName GetAllOrderForOneDay
+   * @apiGroup OrderInfo
+   * @apiPermission admin
+   * 
+   * @apiParam {Date} date Date of order.
+   * 
+   * @apiSuccess (Success 204) NoContent Reponse empty because data is not found in base.
+   * 
+   * @apiSuccessExample Success-Response-Empty :
+   *     HTTP/1.1 204 No Content
+   * 
+   * @apiSuccess {Object[]} data                  List of Order (Array of Objects).
+   * @apiSuccess {Number}   data.id               ID of Order.
+   * @apiSuccess {Number}   data.id_client        ID client linked with the order.   
+   * @apiSuccess {boolean}   data.done             Status of Order.   
+   * @apiSuccess {String}   data.first_name       First name of client.  
+   * @apiSuccess {String}   data.last_name        Last name of client.
+   * 
+   * @apiSuccessExample Success-Response-with data (example) :
+   *     HTTP/1.1 200 OK
+   *     [{ 
+   *             "id" : 1 ,
+   *             "id_client": 1, 
+   *             "done" : false, 
+   *             "first_name": "Louis", 
+   *             "last_name" : "PASTATA" 
+   *       }]
+   * 
+   * 
+   * @apiError {String} MissingFields The date is missing or the date is not formated.
+   * 
+   * @apiErrorExample {json} 400-Error-Response:
+   *     HTTP/1.1 400 Bad Request
+   *     {
+   *       "error": "Missing Fields or Bad format"
+   *     }
+   * 
+   * @apiUse OrderInfoFatalError
+   * 
+   */
   public async getAllOrderForOneDay (req: Request,res: Response) : Promise<void> {
     log.info("Get all Order for one day");
     
-    var regexp = RegExp("^([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])$");
+    let exp = RegExp("^([\\d][\\d][\\d][\\d]-[\\d][\\d]-[\\d][\\d])$");
+    let regexp = new RegExp (exp);
 
     if (!regexp.test(req.params.date) || req.params.date.length != 10 ) 
       {
-            res.status(400).json({ error : "Missing Fields or Bad format" });
-            res.end();
+            res.status(400).json({ error : "Missing Fields or Bad format" }).end();
             log.error("Get All Order for one day : Fail - Missing Fields or Bad format");      
       }
     else
@@ -352,7 +587,6 @@ export class OrderInfoController {
               log.error(error);
           });
 
-          res.status(204).end();
       }
 
     }
